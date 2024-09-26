38351a158e09f318fcb7fa76d066321fb621c937
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-26 12:59:10 +0100

    adjust brand name

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const header = (
   <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
     <Link href="/">
       <h1 className={"text-base sm:text-lg textGradient " + fugazOne.className}>
-        Broodl
+        Moodly
       </h1>
     </Link>
     <div className=" flex items-center justify-between">
